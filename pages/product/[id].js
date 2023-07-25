c222bf96fa18188a15f066260a68147f98c8ebb2
Kailash Chandra <chandrakailash1993@gmail.com>
2023-07-25 11:29:19 +0530

    removed dynamic import

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,5 +1,4 @@
-import dynamic from "next/dynamic";
-const PDPPage = dynamic(() => import("product/plp"), { ssr: true });
+import PDPPage from "product/plp";
 const PDP = PDPPage;
 PDP.getInitialProps = PDPPage.getInitialProps;
 export default PDP;
